0f141b397a87518f4623a2e0c4ee8102bcb1d99e
Arkadiusz Surowka <95arek95@gmail.com>
2023-11-18 09:47:32 +0100

    remove useless components and logout if unauthorized

--- ClientApp/src/app/services/error-handler.service.ts
+++ ClientApp/src/app/services/error-handler.service.ts
@@ -1,19 +1,26 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   handleError(error: HttpErrorResponse){
     if (error.status === 404) {
       this.router.navigate(['not-found']);
     }
     else if (error.status === 401) {
+      const expiration = this.authService.expirationDate();
+
+      if(expiration && expiration < new Date()) {
+        localStorage.clear();
+      }
+
       this.router.navigate(['access-denied']);
     }
 
