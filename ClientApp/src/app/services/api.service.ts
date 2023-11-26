e9c84909dfebab99e279e9216026ad3f92b51d3e
Arkadiusz Surowka <95arek95@gmail.com>
2023-11-26 19:43:07 +0100

    improve angular services

--- ClientApp/src/app/services/api.service.ts
+++ ClientApp/src/app/services/api.service.ts
@@ -3,20 +3,19 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { ErrorHandlerService } from './error-handler.service';
-import { catchError, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService<T>{
     private baseUrl: string = window.location.origin;
-    private objectsSubject = new BehaviorSubject<T[]>([]);
-    objects$: Observable<T[]> = this.objectsSubject.asObservable();
+    private objectsSubject = new BehaviorSubject<T[]>(null);
 
     constructor(private httpClient: HttpClient, private authService: AuthService, private errorHandler: ErrorHandlerService) {
     }
 
-    getList(controllerName: string): void {
+    private fetchData(controllerName: string): void {
         this.httpClient.get<T[]>(this.baseUrl + `/${controllerName}/GetList`, { headers: this.authService.getHeaders() })
             .subscribe(
                 success => this.objectsSubject.next(success),
@@ -24,12 +23,35 @@ export class ApiService<T>{
             );
     }
 
-    addItem(item: T, controllerName: string): Observable<T> {
-        return this.httpClient.post<T>(this.baseUrl + `/${controllerName}/Add`, item, { headers: this.authService.getHeaders() });
+    getList(controllerName: string): Observable<T[]> {
+
+        if(this.objectsSubject.getValue() == null){
+            this.fetchData(controllerName);
+        }
+        return this.objectsSubject.asObservable();
     }
 
-    editItem(id: string, item: T, controllerName: string): Observable<T> {
-        return this.httpClient.post<T>(this.baseUrl + `/${controllerName}/Edit/${id}`, item, { headers: this.authService.getHeaders() });
+    addItem(item: T, controllerName: string): Observable<T> {
+        return this.httpClient.post<T>(this.baseUrl + `/${controllerName}/Add`, item, { headers: this.authService.getHeaders() })
+        .pipe(
+            tap(() => {
+                const currentList = this.objectsSubject.value;
+                currentList.push(item);                  
+                this.objectsSubject.next(currentList);
+            }));
+    }
+
+    editItem(id: string, item: T, controllerName: string, idPropName: string): Observable<T> {
+        return this.httpClient.post<T>(this.baseUrl + `/${controllerName}/Edit/${id}`, item, { headers: this.authService.getHeaders() })
+        .pipe(
+            tap(() => {
+                const currentList = this.objectsSubject.value;
+                let updated = currentList.find(x => x[idPropName] == +id);
+                let index = currentList.indexOf(updated);
+                currentList[index] = item;
+                   
+                this.objectsSubject.next(currentList);
+            }));
     }
 
     getItem(id: string, controllerName: string): Observable<T> {
