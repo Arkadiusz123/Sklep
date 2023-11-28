46eb1e827c4563a44111aa644a9b75613f0c65d0
Arkadiusz Surowka <95arek95@gmail.com>
2023-11-28 19:21:18 +0100

    user register

--- Auth/RegisterModel.cs
+++ Auth/RegisterModel.cs
@@ -13,5 +13,8 @@ namespace Sklep.Auth
 
         [Required]
         public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
     }
 }
--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ValidMessagesService } from '../services/valid-messages.service';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   private baseUrl: string = window.location.origin;
   private controllerName: string = "Authenticate";
+  private subscriptions: Subscription[] = [];
 
   model: Partial<User> = {};
   serverErrors: string = "";
+  register: boolean = false;
+  rolesList: Observable<Role[]>;
 
-
-  constructor(private httpClient: HttpClient, private router: Router, public validMessages: ValidMessagesService) { }
+  constructor(private httpClient: HttpClient, private router: Router, public validMessages: ValidMessagesService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.model.role = "";
+    const sub = this.route.data.subscribe(v => this.register = v.register);
+    this.subscriptions.push(sub);
+    this.rolesList = this.httpClient.get<Role[]>(this.baseUrl + `/${this.controllerName}/GetRolesList`);
   }
 
   logIn() {
-    this.httpClient.post(this.baseUrl + `/${this.controllerName}/Login`, this.model as User).subscribe(
+    const subscription = this.httpClient.post(this.baseUrl + `/${this.controllerName}/Login`, this.model as User)
+    .subscribe(
       success => {
         const token = JSON.stringify(success);
         localStorage.setItem("token", JSON.stringify(success));
@@ -35,6 +43,29 @@ export class LoginComponent implements OnInit {
         }, 4000);
       }
     );
+    this.subscriptions.push(subscription);
+  }
+
+  registerUser(){
+    const subscription = this.httpClient.post(this.baseUrl + `/${this.controllerName}/Register`, this.model as User)
+    .subscribe(
+      () => { this.router.navigate(['log-in']) },
+      () => {
+        this.serverErrors = "Niepoprawne dane"
+        setTimeout(() => {
+          this.serverErrors = '';
+        }, 4000);
+      }
+    );
+    this.subscriptions.push(subscription);
+  }
+  
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    });
   }
 
 }
@@ -42,4 +73,11 @@ export class LoginComponent implements OnInit {
 export interface User {
   username: number;
   password: string;
+  email?: string;
+  role: string;
+}
+
+export interface Role{
+  name: string;
+  value: string;
 }
