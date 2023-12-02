8f8a983b1891d27ba9e051a76e6fd33ae24450c9
Arkadiusz Surowka <95arek95@gmail.com>
2023-12-02 11:43:07 +0100

    add unit test

---
+++ SklepTests/TranslatorTester.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sklep.Tools;
+
+namespace SklepTests
+{
+    [TestClass]
+    public class TranslatorTester
+    {
+        [TestMethod]
+        [DataRow("This field is required", "To pole jest wymagane", "pl")]
+        [DataRow("Invalid data format", "Nieprawidłowy format danych", "pl")]
+        public void Translate(string input, string expected, string targetLang)
+        {
+            var result = Translator.Translate(input, targetLang);
+
+            Assert.AreEqual(expected, result.ResultValue);
+        }
+    }
+}
